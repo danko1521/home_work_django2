3cb1e0e9bec37d70ceba636ddd681afbb457e94a
dangeon master 300$ <vip.danko1521@mail.ru>
2021-08-01 14:57:51 +0300

    + лендинг :0

--- venv/request-handling/landing/app/views.py
+++ venv/request-handling/landing/app/views.py
@@ -6,12 +6,19 @@ from django.shortcuts import render
 # в качестве хранилища количества показов и количества переходов.
 # но помните, что в реальных проектах так не стоит делать
 # так как при перезапуске приложения они обнулятся
-counter_show = Counter()
-counter_click = Counter()
+counter_show = {'original':0, 'test':0}
+counter_click ={'original':0, 'test':0}
 
 
 def index(request):
     # Реализуйте логику подсчета количества переходов с лендига по GET параметру from-landing
+    click = request.GET.get('from-landing', 'index.html')
+    if click == 'original':
+        counter_click['original'] +=1
+        return render(request, 'index.html')
+
+    elif click == 'test':
+        counter_click['test'] +=1
     return render(request, 'index.html')
 
 
@@ -20,13 +27,24 @@ def landing(request):
     # в зависимости от GET параметра ab-test-arg
     # который может принимать значения original и test
     # Так же реализуйте логику подсчета количества показов
-    return render(request, 'landing.html')
+
+    ab_test_arg = request.GET.get('ab-test-arg', index(request))
+    if ab_test_arg == 'original':
+        counter_show['original'] +=1
+        return render(request, 'landing.html')
+
+    elif ab_test_arg == 'test':
+        counter_show['test'] +=1
+        return render(request, 'landing_alternate.html',)
+
+
 
 
 def stats(request):
     # Реализуйте логику подсчета отношения количества переходов к количеству показов страницы
     # Для вывода результат передайте в следующем формате:
     return render(request, 'stats.html', context={
-        'test_conversion': 0.5,
-        'original_conversion': 0.4,
+
+        'test_conversion': counter_click['test'] / counter_show['test'],
+        'original_conversion': counter_click['original'] / counter_show['original']
     })
