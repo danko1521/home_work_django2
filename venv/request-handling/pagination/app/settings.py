1b9ea2dff66ef3268060320101e1c09241ec5c55
dangeon master 300$ <vip.danko1521@mail.ru>
2021-08-03 20:03:26 +0300

    paginator :0

--- venv/request-handling/pagination/app/settings.py
+++ venv/request-handling/pagination/app/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
-import os
+import os, csv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -76,6 +76,10 @@ STATICFILES_DIRS = [
 ]
 
 BUS_STATION_CSV = os.path.join(BASE_DIR, 'data-398-2018-08-30.csv')
+
+
+ITEMS_PER_PAGE = 10
+
 
 SECRET_KEY = 'put your secret here'
 DEBUG = True
--- venv/request-handling/pagination/app/views.py
+++ venv/request-handling/pagination/app/views.py
@@ -1,19 +1,48 @@
+import csv
+import urllib
+
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.urls import reverse
+
+rows = []
 
 
 def index(request):
     return redirect(reverse(bus_stations))
 
 
-def bus_stations(request):
-    current_page = 1
-    next_page_url = 'write your url'
-    return render(request, 'index.html', context={
-        'bus_stations': [{'Name': 'название', 'Street': 'улица', 'District': 'район'},
-                         {'Name': 'другое название', 'Street': 'другая улица', 'District': 'другой район'}],
-        'current_page': current_page,
-        'prev_page_url': None,
-        'next_page_url': next_page_url,
-    })
+def read_cvs(file_name):
+    rows = []
+    with open(file_name, 'r', encoding="cp1251") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            rows.append(row)
+    return rows
 
+
+def bus_stations(request):
+    CONTENT = read_cvs('data-398-2018-08-30.csv')
+
+    paginator = Paginator(CONTENT, 10)
+    page_number = int(request.GET.get('page', 1))
+    page_obj = paginator.get_page(page_number)
+    current_page = page_obj.number
+
+    prev_page_url=None
+    next_page_url=None
+
+    url = f'{reverse(bus_stations)}?'
+    if page_obj.has_previous():
+
+       prev_page_url = url+urllib.parse.urlencode({'page': page_obj.previous_page_number()})
+    if page_obj.has_next():
+
+        next_page_url = url+urllib.parse.urlencode({'page':page_obj.next_page_number()})
+
+
+    return render(request, 'index.html', context={
+        'bus_stations': page_obj,
+        'current_page': current_page,
+        'prev_page_url': prev_page_url,
+        'next_page_url': next_page_url,})
